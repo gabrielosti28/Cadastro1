830fb0eccc0c9e1743dc0942f0b4944f9bd87967
gabrielosti28 <gabriel.osti@aluno.ifsp.edu.br>
2026-01-28 14:47:08 -0300

    viual do todos o clientes atualizado

--- Cadastro1/FormListaClientes.Designer.cs
+++ Cadastro1/FormListaClientes.Designer.cs
@@ -28,13 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panelContainer = new System.Windows.Forms.Panel();
             this.dgvClientes = new System.Windows.Forms.DataGridView();
             this.lblTitulo = new System.Windows.Forms.Label();
             this.btnFechar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.panelContainer.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
             this.SuspendLayout();
@@ -42,6 +43,7 @@
             // panelContainer
             // 
             this.panelContainer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
+            this.panelContainer.Controls.Add(this.label1);
             this.panelContainer.Controls.Add(this.dgvClientes);
             this.panelContainer.Controls.Add(this.lblTitulo);
             this.panelContainer.Controls.Add(this.btnFechar);
@@ -55,31 +57,31 @@
             // 
             this.dgvClientes.AllowUserToAddRows = false;
             this.dgvClientes.AllowUserToDeleteRows = false;
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
-            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(5);
-            this.dgvClientes.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            dataGridViewCellStyle4.Padding = new System.Windows.Forms.Padding(5);
+            this.dgvClientes.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
             this.dgvClientes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvClientes.BackgroundColor = System.Drawing.Color.White;
             this.dgvClientes.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvClientes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvClientes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
             this.dgvClientes.ColumnHeadersHeight = 40;
             this.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F);
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle3.Padding = new System.Windows.Forms.Padding(5);
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvClientes.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI", 11F);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle6.Padding = new System.Windows.Forms.Padding(5);
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvClientes.DefaultCellStyle = dataGridViewCellStyle6;
             this.dgvClientes.EnableHeadersVisualStyles = false;
             this.dgvClientes.Location = new System.Drawing.Point(12, 80);
             this.dgvClientes.MultiSelect = false;
@@ -96,7 +98,7 @@
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
             this.lblTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
-            this.lblTitulo.Location = new System.Drawing.Point(453, 20);
+            this.lblTitulo.Location = new System.Drawing.Point(453, 0);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Size = new System.Drawing.Size(315, 37);
             this.lblTitulo.TabIndex = 1;
@@ -116,6 +118,16 @@
             this.btnFechar.Text = "✖ FECHAR";
             this.btnFechar.UseVisualStyleBackColor = false;
             this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(392, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(448, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Clique duas vezes para ver mais informações";
             // 
             // FormListaClientes
             // 
@@ -142,5 +154,6 @@
         private System.Windows.Forms.DataGridView dgvClientes;
         private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Cadastro1/FormListaClientes.cs
+++ Cadastro1/FormListaClientes.cs
@@ -43,118 +43,171 @@ namespace Cadastro1
                 dgvClientes.DataSource = null;
                 dgvClientes.DataSource = clientes;
 
-                // Configurar colunas existentes
-                if (dgvClientes.Columns.Contains("ClienteID"))
-                {
-                    dgvClientes.Columns["ClienteID"].HeaderText = "CÓDIGO";
-                    dgvClientes.Columns["ClienteID"].Width = 80;
-                }
+                // ===== CONFIGURAR ORDEM E VISIBILIDADE DAS COLUNAS =====
 
+                // 1. OCULTAR colunas que não queremos mostrar
+                if (dgvClientes.Columns.Contains("ClienteID"))
+                    dgvClientes.Columns["ClienteID"].Visible = false;
+
+                if (dgvClientes.Columns.Contains("Ativo"))
+                    dgvClientes.Columns["Ativo"].Visible = false;
+
+                if (dgvClientes.Columns.Contains("CEP"))
+                    dgvClientes.Columns["CEP"].Visible = false;
+
+                if (dgvClientes.Columns.Contains("Telefone"))
+                    dgvClientes.Columns["Telefone"].Visible = false;
+
+                if (dgvClientes.Columns.Contains("BeneficioINSS2"))
+                    dgvClientes.Columns["BeneficioINSS2"].Visible = false;
+
+                if (dgvClientes.Columns.Contains("Endereco"))
+                    dgvClientes.Columns["Endereco"].Visible = false;
+
+                // 2. REMOVER botões se já existirem (para evitar duplicação ao recarregar)
+                if (dgvClientes.Columns.Contains("btnDocumentos"))
+                    dgvClientes.Columns.Remove("btnDocumentos");
+
+                if (dgvClientes.Columns.Contains("btnEditar"))
+                    dgvClientes.Columns.Remove("btnEditar");
+
+                if (dgvClientes.Columns.Contains("btnCodigo"))
+                    dgvClientes.Columns.Remove("btnCodigo");
+
+                // 3. CONFIGURAR colunas de dados existentes
                 if (dgvClientes.Columns.Contains("NomeCompleto"))
                 {
                     dgvClientes.Columns["NomeCompleto"].HeaderText = "NOME COMPLETO";
+                    dgvClientes.Columns["NomeCompleto"].DisplayIndex = 0;
+                    dgvClientes.Columns["NomeCompleto"].Width = 300;
                 }
 
                 if (dgvClientes.Columns.Contains("CPF"))
                 {
                     dgvClientes.Columns["CPF"].HeaderText = "CPF";
+                    dgvClientes.Columns["CPF"].DisplayIndex = 1;
                     dgvClientes.Columns["CPF"].Width = 120;
+                    dgvClientes.Columns["CPF"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
                 }
 
                 if (dgvClientes.Columns.Contains("DataNascimento"))
                 {
                     dgvClientes.Columns["DataNascimento"].HeaderText = "NASCIMENTO";
-                    dgvClientes.Columns["DataNascimento"].Width = 130;
+                    dgvClientes.Columns["DataNascimento"].DisplayIndex = 2;
+                    dgvClientes.Columns["DataNascimento"].Width = 110;
                     dgvClientes.Columns["DataNascimento"].DefaultCellStyle.Format = "dd/MM/yyyy";
-                }
-
-                if (dgvClientes.Columns.Contains("Endereco"))
-                {
-                    dgvClientes.Columns["Endereco"].HeaderText = "ENDEREÇO";
+                    dgvClientes.Columns["DataNascimento"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
                 }
 
                 if (dgvClientes.Columns.Contains("Cidade"))
                 {
                     dgvClientes.Columns["Cidade"].HeaderText = "CIDADE";
+                    dgvClientes.Columns["Cidade"].DisplayIndex = 3;
                     dgvClientes.Columns["Cidade"].Width = 150;
                 }
 
                 if (dgvClientes.Columns.Contains("BeneficioINSS"))
                 {
                     dgvClientes.Columns["BeneficioINSS"].HeaderText = "BENEFÍCIO INSS";
-                    dgvClientes.Columns["BeneficioINSS"].Width = 140;
+                    dgvClientes.Columns["BeneficioINSS"].DisplayIndex = 4;
+                    dgvClientes.Columns["BeneficioINSS"].Width = 130;
+                    dgvClientes.Columns["BeneficioINSS"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
                 }
 
                 if (dgvClientes.Columns.Contains("DataCadastro"))
                 {
                     dgvClientes.Columns["DataCadastro"].HeaderText = "CADASTRADO EM";
-                    dgvClientes.Columns["DataCadastro"].Width = 150;
+                    dgvClientes.Columns["DataCadastro"].DisplayIndex = 5;
+                    dgvClientes.Columns["DataCadastro"].Width = 140;
                     dgvClientes.Columns["DataCadastro"].DefaultCellStyle.Format = "dd/MM/yyyy HH:mm";
+                    dgvClientes.Columns["DataCadastro"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
                 }
 
-                if (dgvClientes.Columns.Contains("Ativo"))
+                // 4. ADICIONAR coluna de CÓDIGO (informativa)
+                DataGridViewTextBoxColumn colCodigo = new DataGridViewTextBoxColumn
                 {
-                    dgvClientes.Columns["Ativo"].Visible = false;
-                }
-
-                if (dgvClientes.Columns.Contains("CEP"))
-                {
-                    dgvClientes.Columns["CEP"].Visible = false;
-                }
-
-                if (dgvClientes.Columns.Contains("Telefone"))
-                {
-                    dgvClientes.Columns["Telefone"].Visible = false;
-                }
-
-                if (dgvClientes.Columns.Contains("BeneficioINSS2"))
-                {
-                    dgvClientes.Columns["BeneficioINSS2"].Visible = false;
-                }
-
-                // Adicionar coluna de botão para editar
-                if (!dgvClientes.Columns.Contains("btnEditar"))
-                {
-                    DataGridViewButtonColumn btnEditar = new DataGridViewButtonColumn
+                    Name = "colCodigo",
+                    HeaderText = "CÓDIGO",
+                    Width = 80,
+                    ReadOnly = true,
+                    DefaultCellStyle = new DataGridViewCellStyle
                     {
-                        Name = "btnEditar",
-                        HeaderText = "EDITAR",
-                        Text = "✏️ Editar",
-                        UseColumnTextForButtonValue = true,
-                        Width = 100
-                    };
-                    dgvClientes.Columns.Insert(0, btnEditar);
-                }
+                        Alignment = DataGridViewContentAlignment.MiddleCenter,
+                        BackColor = Color.FromArgb(236, 240, 241),
+                        ForeColor = Color.FromArgb(52, 73, 94),
+                        Font = new Font("Consolas", 9, FontStyle.Bold)
+                    }
+                };
+                dgvClientes.Columns.Add(colCodigo);
+                colCodigo.DisplayIndex = 6;
 
-                // Adicionar coluna de botão para anexos
-                if (!dgvClientes.Columns.Contains("btnAnexos"))
+                // Preencher valores da coluna de código
+                foreach (DataGridViewRow row in dgvClientes.Rows)
                 {
-                    DataGridViewButtonColumn btnAnexos = new DataGridViewButtonColumn
+                    if (row.DataBoundItem != null)
                     {
-                        Name = "btnAnexos",
-                        HeaderText = "DOCUMENTOS",
-                        Text = "📎 Ver",
-                        UseColumnTextForButtonValue = true,
-                        Width = 100
-                    };
-                    dgvClientes.Columns.Insert(1, btnAnexos);
-                }
-
-                // Adicionar evento de clique no botão
-                dgvClientes.CellClick -= DgvClientes_CellClick; // Remover se já existir
-                dgvClientes.CellClick += DgvClientes_CellClick;
-
-                // Centralizar cabeçalhos e algumas colunas
-                foreach (DataGridViewColumn coluna in dgvClientes.Columns)
-                {
-                    if (coluna.HeaderText.Contains("CÓDIGO") ||
-                        coluna.HeaderText.Contains("CPF") ||
-                        coluna.HeaderText.Contains("NASCIMENTO") ||
-                        coluna.HeaderText.Contains("CADASTRADO"))
-                    {
-                        coluna.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                        Cliente cliente = (Cliente)row.DataBoundItem;
+                        row.Cells["colCodigo"].Value = cliente.ClienteID.ToString("D5"); // Formata como 00001, 00002, etc
                     }
                 }
+
+                // 5. ADICIONAR botão de DOCUMENTOS
+                DataGridViewButtonColumn btnDocumentos = new DataGridViewButtonColumn
+                {
+                    Name = "btnDocumentos",
+                    HeaderText = "DOCUMENTOS",
+                    Text = "📎 Ver",
+                    UseColumnTextForButtonValue = true,
+                    Width = 110,
+                    DefaultCellStyle = new DataGridViewCellStyle
+                    {
+                        BackColor = Color.FromArgb(52, 152, 219),
+                        ForeColor = Color.White,
+                        SelectionBackColor = Color.FromArgb(41, 128, 185),
+                        SelectionForeColor = Color.White,
+                        Font = new Font("Segoe UI", 9, FontStyle.Bold)
+                    }
+                };
+                dgvClientes.Columns.Add(btnDocumentos);
+                btnDocumentos.DisplayIndex = 7;
+
+                // 6. ADICIONAR botão de EDITAR
+                DataGridViewButtonColumn btnEditar = new DataGridViewButtonColumn
+                {
+                    Name = "btnEditar",
+                    HeaderText = "EDITAR",
+                    Text = "✏️ Editar",
+                    UseColumnTextForButtonValue = true,
+                    Width = 100,
+                    DefaultCellStyle = new DataGridViewCellStyle
+                    {
+                        BackColor = Color.FromArgb(230, 126, 34),
+                        ForeColor = Color.White,
+                        SelectionBackColor = Color.FromArgb(211, 84, 0),
+                        SelectionForeColor = Color.White,
+                        Font = new Font("Segoe UI", 9, FontStyle.Bold)
+                    }
+                };
+                dgvClientes.Columns.Add(btnEditar);
+                btnEditar.DisplayIndex = 8;
+
+                // 7. ADICIONAR evento de clique
+                dgvClientes.CellClick -= DgvClientes_CellClick; // Remove se já existir
+                dgvClientes.CellClick += DgvClientes_CellClick;
+
+                // 8. ESTILIZAÇÃO adicional
+                dgvClientes.EnableHeadersVisualStyles = false;
+                dgvClientes.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(52, 73, 94);
+                dgvClientes.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+                dgvClientes.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+                dgvClientes.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                dgvClientes.ColumnHeadersHeight = 40;
+
+                // Alternating row colors para melhor legibilidade
+                dgvClientes.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(236, 240, 241);
+
+                // Atualizar título com contagem
+                lblTitulo.Text = $"📋 TODOS OS CLIENTES ({clientes.Count})";
             }
             catch (Exception ex)
             {
@@ -165,13 +218,32 @@ namespace Cadastro1
 
         private void DgvClientes_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            // Verificar se clicou em algum botão
+            // Verificar se clicou em alguma célula válida
             if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
             {
                 Cliente cliente = (Cliente)dgvClientes.Rows[e.RowIndex].DataBoundItem;
+                string nomeColuna = dgvClientes.Columns[e.ColumnIndex].Name;
 
-                // Botão Editar
-                if (dgvClientes.Columns[e.ColumnIndex].Name == "btnEditar")
+                // Botão DOCUMENTOS
+                if (nomeColuna == "btnDocumentos")
+                {
+                    try
+                    {
+                        FormAnexosCliente formAnexos = new FormAnexosCliente(cliente);
+                        formAnexos.ShowDialog();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(
+                            "Erro ao abrir documentos:\n\n" + ex.Message,
+                            "Erro",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error
+                        );
+                    }
+                }
+                // Botão EDITAR
+                else if (nomeColuna == "btnEditar")
                 {
                     try
                     {
@@ -180,6 +252,13 @@ namespace Cadastro1
                         {
                             // Recarregar lista após edição
                             CarregarClientes();
+
+                            MessageBox.Show(
+                                "✓ Cliente atualizado com sucesso!",
+                                "Sucesso",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information
+                            );
                         }
                     }
                     catch (Exception ex)
@@ -192,25 +271,84 @@ namespace Cadastro1
                         );
                     }
                 }
-                // Botão Anexos
-                else if (dgvClientes.Columns[e.ColumnIndex].Name == "btnAnexos")
+                // Clique em qualquer outra célula - mostrar detalhes rápidos
+                else
                 {
-                    try
-                    {
-                        FormAnexosCliente formAnexos = new FormAnexosCliente(cliente);
-                        formAnexos.ShowDialog();
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(
-                            "Erro ao abrir anexos:\n\n" + ex.Message,
-                            "Erro",
-                            MessageBoxButtons.OK,
-                            MessageBoxIcon.Error
-                        );
-                    }
+                    MostrarDetalhesRapidos(cliente);
                 }
             }
+        }
+
+        private void MostrarDetalhesRapidos(Cliente cliente)
+        {
+            try
+            {
+                int idade = ClienteDAL.CalcularIdade(cliente.DataNascimento);
+
+                string detalhes = $"📋 INFORMAÇÕES DO CLIENTE\n\n";
+                detalhes += $"━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n";
+                detalhes += $"👤 Nome: {cliente.NomeCompleto}\n\n";
+                detalhes += $"🆔 CPF: {FormatarCPF(cliente.CPF)}\n\n";
+                detalhes += $"🎂 Nascimento: {cliente.DataNascimento:dd/MM/yyyy} ({idade} anos)\n\n";
+                detalhes += $"🏠 Endereço: {cliente.Endereco}\n\n";
+                detalhes += $"🏙️ Cidade: {cliente.Cidade}\n\n";
+                detalhes += $"📮 CEP: {FormatarCEP(cliente.CEP)}\n\n";
+
+                if (!string.IsNullOrEmpty(cliente.Telefone))
+                    detalhes += $"📞 Telefone: {cliente.Telefone}\n\n";
+
+                detalhes += $"💼 INSS: {cliente.BeneficioINSS}\n\n";
+
+                if (!string.IsNullOrEmpty(cliente.BeneficioINSS2))
+                    detalhes += $"💼 2º INSS: {cliente.BeneficioINSS2}\n\n";
+
+                detalhes += $"📅 Cadastrado em: {cliente.DataCadastro:dd/MM/yyyy HH:mm}\n\n";
+                detalhes += $"🔢 Código: {cliente.ClienteID:D5}";
+
+                MessageBox.Show(
+                    detalhes,
+                    "Detalhes do Cliente",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    $"Erro ao exibir detalhes:\n\n{ex.Message}",
+                    "Erro",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+        }
+
+        private string FormatarCPF(string cpf)
+        {
+            if (string.IsNullOrEmpty(cpf)) return "";
+
+            cpf = cpf.Replace("-", "").Replace(".", "").Trim();
+
+            if (cpf.Length == 11)
+            {
+                return $"{cpf.Substring(0, 3)}.{cpf.Substring(3, 3)}.{cpf.Substring(6, 3)}-{cpf.Substring(9, 2)}";
+            }
+
+            return cpf;
+        }
+
+        private string FormatarCEP(string cep)
+        {
+            if (string.IsNullOrEmpty(cep)) return "";
+
+            cep = cep.Replace("-", "").Trim();
+
+            if (cep.Length == 8)
+            {
+                return $"{cep.Substring(0, 5)}-{cep.Substring(5, 3)}";
+            }
+
+            return cep;
         }
 
         private void btnFechar_Click(object sender, EventArgs e)
